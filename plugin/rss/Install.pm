27afadaa57728448545c3d75daee2f249399d94c
hiroyuki.nagata <idiotpanzer@gmail.com>
2022-04-12 23:34:57 +0900

    rss-jp.netはサービス停止しているので削除 #37

--- plugin/rss/Install.pm
+++ plugin/rss/Install.pm
@@ -10,7 +10,7 @@ use warnings;
 sub install {
 	my $wiki = shift;
 	$wiki->add_menu("RSS",$wiki->create_url({action=>"RSS"}),50,1);
-	
+
 	if($wiki->config("rss_version") eq "1"){
 		$wiki->add_handler("RSS","plugin::rss::RSSMaker10");
 		$wiki->add_hook("save_after" ,"plugin::rss::RSSMaker10");
@@ -22,9 +22,8 @@ sub install {
 		$wiki->add_hook("delete"     ,"plugin::rss::RSSMaker");
 		$wiki->add_hook("initialize" ,"plugin::rss::RSSMaker");
 	}
-	
+
 	$wiki->add_paragraph_plugin("rss" ,"plugin::rss::RSS" ,"HTML");
-#	$wiki->add_paragraph_plugin("rss2","plugin::rss::RSS2","HTML");
 }
 
 1;
