f02ef54532d6311dce63ceda7e8ccf46826d2114
hiroyuki.nagata <idiotpanzer@gmail.com>
2023-05-19 00:01:00 +0900

    https://github.com/MichaelCurrin/deno-project-template を導入

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { parse } from "./deps.ts";
+import { greet } from "./lib.ts";
+
+const args = parse(Deno.args);
+const name = args.name || "World";
+const app = new Application();
+
+app.use((ctx) => {
+    ctx.response.body = name;
+});
+
+await app.listen({ port: 8000 });
+console.log(greet(name));
