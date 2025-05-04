92515c30d5e4375233f159f20bc9c29a6f5d41da
hiroyuki.nagata <idiotpanzer@gmail.com>
2025-05-04 15:14:53 +0900

    古いファイルを削除

--- tools/sample.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo FSWIKI_HOME=%FSWIKI_HOME%
-echo HTML‚ğ¶¬‚µ‚Ü‚·B
-wiki2html.pl readme.wiki -css=default.css -output=sjis > readme.html
-echo PDF‚ğ¶¬‚µ‚Ü‚·B
-wiki2pdf.pl readme.wiki readme.pdf
