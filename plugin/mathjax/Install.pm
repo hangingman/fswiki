a1550fe9238d500e1fc96bc77a2e7a5527e52035
hiroyuki.nagata <idiotpanzer@gmail.com>
2026-06-04 23:12:55 +0900

    fix: remove malicious polyfill.io CDN link
    The polyfill.io service has been compromised and is serving malware.
    Modern browsers no longer require this polyfill for MathJax.

--- plugin/mathjax/Install.pm
+++ plugin/mathjax/Install.pm
@@ -12,7 +12,6 @@ sub install {
 	$wiki->add_inline_plugin("mathjax","plugin::mathjax::MathJax", "HTML");
 
 	my $head_info = <<"END_OF_HEAD";
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async
   src="https://cdn.jsdelivr.net/npm/mathjax\@3/es5/tex-mml-chtml.js">
 </script>
